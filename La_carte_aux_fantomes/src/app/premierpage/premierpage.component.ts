4591685c1da3355a9841715b4ed64a69b2775f1e
Martin Raphael <martin.raph@live.fr>
2022-02-08 16:33:16 +0100

    mesg avertisement + instalation APImapbox

--- La_carte_aux_fantomes/src/app/premierpage/premierpage.component.ts
+++ La_carte_aux_fantomes/src/app/premierpage/premierpage.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+
+ 
+
+
+
 @Component({
   selector: 'app-premierpage',
   templateUrl: './premierpage.component.html',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PremierpageComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor() { 
+    
+
+  }
 
   ngOnInit(): void {
+      this.initMap()
+  }
+
+  initMap(){
+    
+        var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+        
+        mapboxgl.accessToken = 'pk.eyJ1IjoicmFwaGFlbDMxODYiLCJhIjoiY2t6ZTVub3Q0Mmc1cjJ2bnhnYWNqb2sxbSJ9.1_BvoAslFg2Qp7aOaB8g9w';
+        var map = new mapboxgl.Map({
+          container: 'map',
+          style: 'mapbox://styles/mapbox/streets-v11',
+          center: [1.7,47.581,-0.857],
+          zoom: 4.25,
+    });
   }
 
 }
